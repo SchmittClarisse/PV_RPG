cc38e2bdc235ec3836938fee17bbc8365a55ddd3
SchmittClarisse <82055525+SchmittClarisse@users.noreply.github.com>
2021-07-08 22:30:13 +0200

    bug stat mana ok

--- Game.js
+++ Game.js
@@ -18,31 +18,31 @@ class Game {
     if(paladin.status == "loser"){
       console.log("Le Paladin " + paladin.name + " est mort... RIP !")
     } else{
-      console.log("Le Paladin " + paladin.name + " à "+ paladin.hp + " HP" + " et " + paladin.Mana + " Mana")
+      console.log("Le Paladin " + paladin.name + " à "+ paladin.hp + " HP" + " et " + paladin.mana + " Mana")
     }
     
     if(monk.status == "loser"){
       console.log("Le Monk " + monk.name + " est mort... RIP !")
     } else{
-      console.log("Le Monk " + monk.name + " à "+ monk.hp + " HP" + " et " + monk.Mana + " Mana")
+      console.log("Le Monk " + monk.name + " à "+ monk.hp + " HP" + " et " + monk.mana + " Mana")
     }
 
     if(assassin.status == "loser"){
       console.log("L'Assassin " + assassin.name + " est mort... RIP !")
     } else{
-      console.log("L'Assassin " + assassin.name + " à "+ assassin.hp + " HP" + " et " + assassin.Mana + " Mana")
+      console.log("L'Assassin " + assassin.name + " à "+ assassin.hp + " HP" + " et " + assassin.mana + " Mana")
     }
 
     if(berzerker.status == "loser"){
       console.log("Le Berzerker " + berzerker.name + " est mort... RIP !")
     } else{
-      console.log("Le Berzerker " + berzerker.name + " à "+ berzerker.hp + " HP" + " et " + berzerker.Mana + " Mana")
+      console.log("Le Berzerker " + berzerker.name + " à "+ berzerker.hp + " HP" + " et " + berzerker.mana + " Mana")
     }
 
     if(fighter.status == "loser"){
       console.log("Le Fighter " + fighter.name + " est mort... RIP !")
     } else{
-      console.log("Le Fighter " + fighter.name + " à "+ fighter.hp + " HP" + " et " + fighter.Mana + " Mana")
+      console.log("Le Fighter " + fighter.name + " à "+ fighter.hp + " HP" + " et " + fighter.mana + " Mana")
     }
   }
 }
